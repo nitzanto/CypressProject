0887a2a70f192153568b3bf89cacfd6b7812e88e
Nitzan <nitzan16001@gmail.com>
2023-08-12 18:20:45 +0300

    Fixed the accordion unit test

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -7,9 +7,9 @@ describe('Fundamentals test', () => {
   it('Accordion works correctly', () => {
     cy.visit('/fundamentals')
     cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
-    cy.get('[data-test="accordion-item-1"]').click()
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
     cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
-    cy.get('[data-test="accordion-item-1"]').click()
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
     cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
   })
 })
