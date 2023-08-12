d4ce0845a9991a426aa842ec25297b36d277a868
Nitzan <nitzan16001@gmail.com>
2023-08-12 19:42:58 +0300

    * Added another unit test to the form page

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -4,9 +4,24 @@ describe('forms tests', () => {
     })
     it('Test subscribe form', () => {
         cy.contains(/testing forms/i)
-        cy.getDataTest('subscribe-form').find('input').type('email@address.com')
+        cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
+        cy.get('@subscribe-input').type('email@address.com')
         cy.contains(/Successfully subbed: email@address.com!/i).should('not.exist')
         cy.getDataTest('subscribe-button').click()
         cy.contains(/Successfully subbed: email@address.com!/i).should('exist')
+        cy.wait(3000)
+        cy.contains(/Successfully subbed: email@address.com!/i).should('not.exist')
+
+        cy.get('@subscribe-input').type('email@address.io')
+        cy.contains(/invalid email: email@address.io/i).should('not.exist')
+        cy.getDataTest('subscribe-button').click()
+        cy.contains(/invalid email: email@address.io/i).should('exist')
+
+        cy.wait(3000)
+        cy.contains(/invalid email: email@address.io/i).should('not.exist')
+
+        cy.contains(/fail!/i).should('not.exist')
+        cy.getDataTest('subscribe-button').click()
+        cy.contains(/fail!/i).should('exist')
     })
 })
