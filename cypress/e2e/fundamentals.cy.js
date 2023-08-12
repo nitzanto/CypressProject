7a1adf7626d9e7b52346af6b1af0d12d11e3f0b7
Nitzan <nitzan16001@gmail.com>
2023-08-12 18:07:33 +0300

    Configured Correct endpoint for testing the app

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,6 +1,6 @@
 describe('Fundamentals test', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/fundamentals')
-    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
+  it('Contains correct header text', () => {
+    cy.visit('/fundamentals')
+    cy.get('[data-test="fundamentals-header"]').should('contain.text', 'Testing Fundamentals')
   })
 })
